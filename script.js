7066349385e7915e5fc6f84433a24d0dacb59280
Kashyap Bhyravavajjula <kashyap.bhvajjula@gmail.com>
2024-01-20 18:41:31 +0530

    Commit 2

--- script.js
+++ script.js
@@ -1,53 +1,48 @@
-/** @format */
-
 let arr = [
+  { id: 1, name: 'john', age: 18, profession: 'developer' },
+  { id: 2, name: 'jack', age: 20, profession: 'developer' },
+  { id: 3, name: 'karen', age: 19, profession: 'admin' },
+];
+  
+function PrintDeveloper() {
+  //Write your code here , just console.log
+  
+  const p = arr.filter((eployee) => {
+    return eployee.profession == "developer";
+  })
+  p.map((data) => {
+    console.log(data)
+  })
+}  
+
+  
+function addData() {
+  //Write your code here, just console.log
+  const newObj = ({ id: 4, name: "susan", age: "20", profession: "intern" })
+  arr.push(newObj)
+  console.log(newObj)
+}
+
+function removeAdmin() {
+  //Write your code here, just console.log
+  const reAdmin = arr.filter((eployee) => {
+    return eployee.profession != "admin";
+  })
+  console.log(reAdmin)
+}
+  
+function concatenateArray() {
+  //Write your code here, just console.log
+  let arr2 = [
     { id: 1, name: "john", age: "18", profession: "developer" },
     { id: 2, name: "jack", age: "20", profession: "developer" },
     { id: 3, name: "karen", age: "19", profession: "admin" },
   ];
-  
-  function PrintDeveloperbyMap() {
-    //Write your code here , just console.log
-    const p = arr.filter((eployee) => {
-      return eployee.profession == "developer";
-    })
-    p.map((data) => {
-      console.log(data)
-    })
-  }
-  
-  function PrintDeveloperbyForEach() {
-    //Write your code here , just console.log
-    const f = arr.filter((eployee) => {
-      return eployee.profession == "developer";
-    })
-    f.forEach((data) => {
-      console.log(data)
-    })
-  }
-  
-  function addData() {
-    //Write your code here, just console.log
-    const newObj = ({ id: 4, name: "susan", age: "20", profession: "intern" })
-    arr.push(newObj)
-    console.log(newObj)
-  }
-  
-  function removeAdmin() {
-    //Write your code here, just console.log
-    const reAdmin = arr.filter((eployee) => {
-      return eployee.profession != "admin";
-    })
-    console.log(reAdmin)
-  }
-  
-  function concatenateArray() {
-    //Write your code here, just console.log
-    let arr2 = [
-      { id: 1, name: "john", age: "18", profession: "developer" },
-      { id: 2, name: "jack", age: "20", profession: "developer" },
-      { id: 3, name: "karen", age: "19", profession: "admin" },
-    ];
-    const newArr = arr.concat(arr2)
-    console.log(newArr)
-  }
+  const newArr = arr.concat(arr2)
+  console.log(newArr)
+}
+
+function consoleArr() {
+  console.log('Consoling Array Variable', arr);
+  // Over here I can directly access the variable.
+}
